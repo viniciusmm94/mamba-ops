e3a380d70edc03b0399f9ce8554d7a2e67c332d6
viniciusmm94 <viniciusmm94l@gmail.com>
2026-04-22 16:08:59 -0300

    switch to google-auth

--- services/sheets.py
+++ services/sheets.py
@@ -1,26 +1,28 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
+from google.oauth2.service_account import Credentials
+
 
 def salvar_no_sheets(dados):
 
     scope = [
-        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
 
     creds_dict = dict(st.secrets["GOOGLE_CREDENTIALS"])
 
-    # 🔥 CORREÇÃO REAL
-    creds_dict["private_key"] = creds_dict["private_key"].replace("\r\n", "\n")
+    credentials = Credentials.from_service_account_info(creds_dict, scopes=scope)
 
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-    client = gspread.authorize(creds)
+    client = gspread.authorize(credentials)
 
     planilha = client.open_by_key("1l4tvrE8A906ctO3xJjlTQx1Lw58yewxTN83cGfZMJ6M")
     aba = planilha.sheet1
 
     aba.clear()
+
+    if not dados:
+        return
 
     header = list(dados[0].keys())
     rows = [list(d.values()) for d in dados]
