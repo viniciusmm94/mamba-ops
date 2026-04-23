f4a16a2bfe30ad864aad4545a25159ebd07b3709
viniciusmm94 <viniciusmm94l@gmail.com>
2026-04-23 16:49:50 -0300

    layout novo feriass

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.markdown(
         font-family: 'Poppins', sans-serif;
         font-weight: 600;
         font-size: 40px;
-        color: #7c3aed;
+        color: #333;
         margin-bottom: 0;
     ">
         Gestão de Jornada
